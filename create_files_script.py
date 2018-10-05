083c19a6da35d5f81cd769b58583b7197a4fbd33
zy lin <zheyuanlin@zys-MacBook-Pro.local>
2018-10-04 21:24:17 -0400

    finishing up

--- create_files_script.py
+++ create_files_script.py
@@ -28,21 +28,19 @@ def main():
 		if os.path.exists(directory) == False:
 			# directory doesn't exist, make one
 			os.mkdir(directory)
-		block_size_list = [128, 512, 1024, 4096, 1024 * 8, 1024 * 16, 1024 * 64, 1024 * 128, 1024 ** 2, 2 * 1024 ** 2]
+		block_size_list = [128, 512, 1024, 4096, 1024 * 8, 1024 * 16, 1024 * 32, 1024 * 64, 1024 * 128, 1024 * 256]
 		# set total_bytes, just a random pick
-		total_bytes = 10 * 1024 ** 2
+		total_bytes = 1024 ** 2
 		writer = csv.DictWriter(sys.stdout, fieldnames=('block size', 'time (milliseconds)'))
 		writer.writeheader()
 
 		for block_size in block_size_list:
-			# 5 files for each block size
-			for i in range(2):
-				created_file = os.path.join(directory, './created_file_{size}_{number}'.format(size=block_size, number=i))
-				# get return from run_create_random_file
-				time_taken = run_create_random_file(created_file, total_bytes, block_size)
-				writer.writerow({'block size': block_size, 'time (milliseconds)': time_taken})
-				# remove file
-				os.remove(created_file)
+			created_file = os.path.join(directory, './created_file_{size}_{number}'.format(size=block_size, number=i))
+			# get return from run_create_random_file
+			time_taken = run_create_random_file(created_file, total_bytes, block_size)
+			writer.writerow({'block size': block_size, 'time (milliseconds)': time_taken})
+			# remove file
+			os.remove(created_file)
 
 
 if __name__ == '__main__':
--- histogram_script.py
+++ histogram_script.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import subprocess
@@ -15,4 +15,23 @@ def run_get_histogram(filename, block_size):
 	output = subprocess.run(args, stdout=subprocess.PIPE)
 	time = output.stdout.split()[-2]
 	return int(time)
+
+def main():
+	if len(sys.argv != 2):
+		print('wrong number of arguments')
+		sys.exit(1)
+	else:
+		filename = sys.argv[1]
+		block_size_list = [128, 512, 1024, 4096, 1024 * 8, 1024 * 16, 1024 * 32, 1024 * 64, 1024 * 128, 1024 * 256]
+		total_bytes = 1024 ** 2
+		writer = csv.DictWriter(sys.stdout, fieldnames=('block size', 'time (milliseconds)'))
+		writer.writeheader()
+
+		for block_size in block_size_list:
+			time_taken = run_get_histogram(filename, block_size)
+			writer.writerow({'block size': block_size, 'time (milliseconds)': time_taken})
+
+if __name__ == '__main__':
+	main()
+
 
